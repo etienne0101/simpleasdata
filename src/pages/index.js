5e2186886dc58f366710aa52afa0727feddb7213
Etienne0101 <etiennepichotdamon@gmail.com>
2022-02-15 18:30:15 +0100

    update css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="Simple comme une donnée">
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   )
