f686cad41148bf090a91e84dcc9c5b9f11f2aa69
Etienne0101 <etiennepichotdamon@gmail.com>
2022-02-16 08:20:45 +0100

    change bio elements

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,10 +44,10 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Réalisé par <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href={`https://linkedin.com/in/${social?.linkedin || ``}`}>
+            Retrouvez moi sur Linkedin
           </a>
         </p>
       )}
