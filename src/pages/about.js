45f400cd62225d7c2f29029babd012e9cb694c04
Etienne0101 <etiennepichotdamon@gmail.com>
2022-02-17 16:18:21 +0100

    change bio and image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Bio from "../components/bio"
+
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
+      <Bio />
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
